c92a25f324b3fa4ff8407058b9725e8afe7dc4c6
Rich Dammkoehler <rpd@noradltd.com>
2015-02-26 13:31:18 -0500

    removed new duplicate guard on QUALITY_CEILING

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -23,14 +23,12 @@ class GildedRose {
 
   private void updateItem(Item item) {
     if (isAgedBrie(item) || isBackstagePass(item)) {
-      if (item.quality < QUALITY_CEILING) {
-        incrementQuality(item);
-      }
+      incrementQuality(item);
       if (isBackstagePass(item)) {
         if (item.sellIn < 11) {
           incrementQuality(item);
         }
-        
+
         if (item.sellIn < 6) {
           incrementQuality(item);
         }

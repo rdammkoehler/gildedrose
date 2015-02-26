913ed29a8efeaf9138a0d6fd64ef13901edde5b9
Rich Dammkoehler <rpd@noradltd.com>
2015-02-26 13:14:20 -0500

    refactor: extracted item update from the for loop

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -15,51 +15,55 @@ class GildedRose {
 
   public void updateQuality() {
     for (Item item : items) {
-      if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-        if (item.quality > 0) {
-          if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-            item.quality = item.quality - 1;
+      updateItem(item);
+    }
+  }
+
+  private void updateItem(Item item) {
+    if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
+      if (item.quality > 0) {
+        if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+          item.quality = item.quality - 1;
+        }
+      }
+    } else {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1;
+
+        if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
+          if (item.sellIn < 11) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
+            }
           }
+
+          if (item.sellIn < 6) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
+            }
+          }
+        }
+      }
+    }
+
+    if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+      item.sellIn = item.sellIn - 1;
+    }
+
+    if (item.sellIn < 0) {
+      if (!item.name.equals(AGED_BRIE)) {
+        if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
+          if (item.quality > 0) {
+            if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+              item.quality = item.quality - 1;
+            }
+          }
+        } else {
+          item.quality = item.quality - item.quality;
         }
       } else {
         if (item.quality < 50) {
           item.quality = item.quality + 1;
-
-          if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-          }
-        }
-      }
-
-      if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-        item.sellIn = item.sellIn - 1;
-      }
-
-      if (item.sellIn < 0) {
-        if (!item.name.equals(AGED_BRIE)) {
-          if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-            if (item.quality > 0) {
-              if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                item.quality = item.quality - 1;
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality;
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
         }
       }
     }

a7a90d2cc93229d0f501e2d42d2ced4723e9a92a
Rich Dammkoehler <rpd@noradltd.com>
2015-02-26 13:22:55 -0500

    refactor: extracted QUALITY_CEILING constant

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,7 @@
 package com.gildedrose;
 
 class GildedRose {
+  private static final int QUALITY_CEILING = 50;
   private static final String AGED_BRIE = "Aged Brie";
   private static final String BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
   private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
@@ -27,18 +28,18 @@ class GildedRose {
         }
       }
     } else {
-      if (item.quality < 50) {
+      if (item.quality < QUALITY_CEILING) {
         incrementQuality(item);
 
         if (isBackstagePass(item)) {
           if (item.sellIn < 11) {
-            if (item.quality < 50) {
+            if (item.quality < QUALITY_CEILING) {
               incrementQuality(item);
             }
           }
 
           if (item.sellIn < 6) {
-            if (item.quality < 50) {
+            if (item.quality < QUALITY_CEILING) {
               incrementQuality(item);
             }
           }
@@ -62,7 +63,7 @@ class GildedRose {
           makeWorthless(item);
         }
       } else {
-        if (item.quality < 50) {
+        if (item.quality < QUALITY_CEILING) {
           incrementQuality(item);
         }
       }

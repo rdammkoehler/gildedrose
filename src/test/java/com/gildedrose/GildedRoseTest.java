3160afa4389bbb02bec5ab87b703f0ca16b1c387
Rich Dammkoehler <rpd@noradltd.com>
2015-02-26 10:53:06 -0500

    refactor: buried GildedRose creation details in helper

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,4 +1,5 @@
 package com.gildedrose;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -6,62 +7,59 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
-    private static final String JUNK = "junk";
-    private static final String AGED_BRIE = "Aged Brie";
-    private GildedRose app;
+  private static final String JUNK = "junk";
+  private static final String AGED_BRIE = "Aged Brie";
+  private GildedRose app;
 
-    @Test
-    public void itemsWhosQualityIsNegativeAreReportedAsQualityZero() {
-      Item[] items = new Item[] { new Item(JUNK, 0, -1)};
-      app = new GildedRose(items);
-      assertThat(app.items[0].quality, is(0));
-    }
-    
-    @Test
-    public void ctItemsWhosQualityBecomesNegativeAreReportedAsQualityZero() {
-      Item[] items = new Item[] { new Item(JUNK, 0, 0)};
-      app = new GildedRose(items);
-      app.updateQuality();
-      assertThat(app.items[0].quality, is(0));
-    }
-    
-    @Test
-    public void ctSpecialItemsWhosQualityIs50CannotGetHigherQuality() {
-      Item[] items = new Item[] { new Item(AGED_BRIE, 0, 50)};
-      app = new GildedRose(items);
-      app.updateQuality();
-      assertThat(app.items[0].quality, is(50));
-    }
-    
-    @Test
-    public void ctSpecialItemsIncreaseInQualityWithAge() {
-      Item[] items = new Item[] { new Item(AGED_BRIE, 11, 40)};
-      app = new GildedRose(items);
-      app.updateQuality();
-      assertThat(app.items[0].quality, is(41));
-    }
-    
-    @Test
-    public void ctAgedBrieIncreasesInQualityByTwoOnItsExpirationDay() {
-      Item[] items = new Item[] { new Item(AGED_BRIE, 0, 40)};
-      app = new GildedRose(items);
-      app.updateQuality();
-      assertThat(app.items[0].quality, is(42));
-    }
-    
-    @Test
-    public void ctAgedBrieIncreasesInQualityByTwoAfterItsExpirationDay() {
-      Item[] items = new Item[] { new Item(AGED_BRIE, -1, 40)};
-      app = new GildedRose(items);
-      app.updateQuality();
-      assertThat(app.items[0].quality, is(42));
-    }
-    
-    @Test
-    public void ctAgedBrieIncreasesInQualityByOneBeforeItsExpirationDay() {
-      Item[] items = new Item[] { new Item(AGED_BRIE, 1, 40)};
-      app = new GildedRose(items);
-      app.updateQuality();
-      assertThat(app.items[0].quality, is(41));
-    }
+  private void initialize(Item... items) {
+    app = new GildedRose(items);
+  }
+
+  @Test
+  public void itemsWhosQualityIsNegativeAreReportedAsQualityZero() {
+    initialize(new Item(JUNK, 0, -1));
+    assertThat(app.items[0].quality, is(0));
+  }
+
+  @Test
+  public void ctItemsWhosQualityBecomesNegativeAreReportedAsQualityZero() {
+    initialize(new Item(JUNK, 0, 0));
+    app.updateQuality();
+    assertThat(app.items[0].quality, is(0));
+  }
+
+  @Test
+  public void ctSpecialItemsWhosQualityIs50CannotGetHigherQuality() {
+    initialize(new Item(AGED_BRIE, 0, 50));
+    app.updateQuality();
+    assertThat(app.items[0].quality, is(50));
+  }
+
+  @Test
+  public void ctSpecialItemsIncreaseInQualityWithAge() {
+    initialize(new Item(AGED_BRIE, 11, 40));
+    app.updateQuality();
+    assertThat(app.items[0].quality, is(41));
+  }
+
+  @Test
+  public void ctAgedBrieIncreasesInQualityByTwoOnItsExpirationDay() {
+    initialize(new Item(AGED_BRIE, 0, 40));
+    app.updateQuality();
+    assertThat(app.items[0].quality, is(42));
+  }
+
+  @Test
+  public void ctAgedBrieIncreasesInQualityByTwoAfterItsExpirationDay() {
+    initialize(new Item(AGED_BRIE, -1, 40));
+    app.updateQuality();
+    assertThat(app.items[0].quality, is(42));
+  }
+
+  @Test
+  public void ctAgedBrieIncreasesInQualityByOneBeforeItsExpirationDay() {
+    initialize(new Item(AGED_BRIE, 1, 40));
+    app.updateQuality();
+    assertThat(app.items[0].quality, is(41));
+  }
 }

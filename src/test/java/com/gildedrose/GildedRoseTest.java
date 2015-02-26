ba287b854a5f637e7eda5e240af9c53da9c81ed1
Rich Dammkoehler <rpd@noradltd.com>
2015-02-26 10:38:37 -0500

    refactor: constants rather than string literals for items

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,16 +6,19 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
+    private static final String JUNK = "junk";
+    private static final String AGED_BRIE = "Aged Brie";
+
     @Test
     public void itemsWhosQualityIsNegativeAreReportedAsQualityZero() {
-      Item[] items = new Item[] { new Item("junk", 0, -1)};
+      Item[] items = new Item[] { new Item(JUNK, 0, -1)};
       GildedRose app = new GildedRose(items);
       assertEquals(0, app.items[0].quality);
     }
     
     @Test
     public void ctItemsWhosQualityBecomesNegativeAreReportedAsQualityZero() {
-      Item[] items = new Item[] { new Item("junk", 0, 0)};
+      Item[] items = new Item[] { new Item(JUNK, 0, 0)};
       GildedRose app = new GildedRose(items);
       app.updateQuality();
       assertEquals(0, app.items[0].quality);
@@ -23,7 +26,7 @@ public class GildedRoseTest {
     
     @Test
     public void ctSpecialItemsWhosQualityIs50CannotGetHigherQuality() {
-      Item[] items = new Item[] { new Item("Aged Brie", 0, 50)};
+      Item[] items = new Item[] { new Item(AGED_BRIE, 0, 50)};
       GildedRose app = new GildedRose(items);
       app.updateQuality();
       assertEquals(50, app.items[0].quality);
@@ -31,7 +34,7 @@ public class GildedRoseTest {
     
     @Test
     public void ctSpecialItemsIncreaseInQualityWithAge() {
-      Item[] items = new Item[] { new Item("Aged Brie", 11, 40)};
+      Item[] items = new Item[] { new Item(AGED_BRIE, 11, 40)};
       GildedRose app = new GildedRose(items);
       app.updateQuality();
       assertEquals(41, app.items[0].quality);
@@ -39,7 +42,7 @@ public class GildedRoseTest {
     
     @Test
     public void ctAgedBrieIncreasesInQualityByTwoOnItsExpirationDay() {
-      Item[] items = new Item[] { new Item("Aged Brie", 0, 40)};
+      Item[] items = new Item[] { new Item(AGED_BRIE, 0, 40)};
       GildedRose app = new GildedRose(items);
       app.updateQuality();
       assertEquals(42, app.items[0].quality);

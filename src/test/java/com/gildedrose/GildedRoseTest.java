14d0f8c2c4802fcdeccb11c3b206b3125a6d6925
Rich Dammkoehler <rpd@noradltd.com>
2015-02-26 10:43:53 -0500

    added characterization of Aged Brie after its expiration

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -47,4 +47,12 @@ public class GildedRoseTest {
       app.updateQuality();
       assertThat(app.items[0].quality, is(42));
     }
+    
+    @Test
+    public void ctAgedBrieIncreasesInQualityByTwoAfterItsExpirationDay() {
+      Item[] items = new Item[] { new Item(AGED_BRIE, -1, 40)};
+      GildedRose app = new GildedRose(items);
+      app.updateQuality();
+      assertThat(app.items[0].quality, is(42));
+    }
 }

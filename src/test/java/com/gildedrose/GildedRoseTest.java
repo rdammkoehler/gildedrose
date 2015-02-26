b6f4a7965eca3729a2bb087d0a09d625f3e23a46
Rich Dammkoehler <rpd@noradltd.com>
2015-02-26 10:47:41 -0500

    refactor: made GuildedRose a member

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,18 +8,19 @@ public class GildedRoseTest {
 
     private static final String JUNK = "junk";
     private static final String AGED_BRIE = "Aged Brie";
+    private GildedRose app;
 
     @Test
     public void itemsWhosQualityIsNegativeAreReportedAsQualityZero() {
       Item[] items = new Item[] { new Item(JUNK, 0, -1)};
-      GildedRose app = new GildedRose(items);
+      app = new GildedRose(items);
       assertThat(app.items[0].quality, is(0));
     }
     
     @Test
     public void ctItemsWhosQualityBecomesNegativeAreReportedAsQualityZero() {
       Item[] items = new Item[] { new Item(JUNK, 0, 0)};
-      GildedRose app = new GildedRose(items);
+      app = new GildedRose(items);
       app.updateQuality();
       assertThat(app.items[0].quality, is(0));
     }
@@ -27,7 +28,7 @@ public class GildedRoseTest {
     @Test
     public void ctSpecialItemsWhosQualityIs50CannotGetHigherQuality() {
       Item[] items = new Item[] { new Item(AGED_BRIE, 0, 50)};
-      GildedRose app = new GildedRose(items);
+      app = new GildedRose(items);
       app.updateQuality();
       assertThat(app.items[0].quality, is(50));
     }
@@ -35,7 +36,7 @@ public class GildedRoseTest {
     @Test
     public void ctSpecialItemsIncreaseInQualityWithAge() {
       Item[] items = new Item[] { new Item(AGED_BRIE, 11, 40)};
-      GildedRose app = new GildedRose(items);
+      app = new GildedRose(items);
       app.updateQuality();
       assertThat(app.items[0].quality, is(41));
     }
@@ -43,7 +44,7 @@ public class GildedRoseTest {
     @Test
     public void ctAgedBrieIncreasesInQualityByTwoOnItsExpirationDay() {
       Item[] items = new Item[] { new Item(AGED_BRIE, 0, 40)};
-      GildedRose app = new GildedRose(items);
+      app = new GildedRose(items);
       app.updateQuality();
       assertThat(app.items[0].quality, is(42));
     }
@@ -51,7 +52,7 @@ public class GildedRoseTest {
     @Test
     public void ctAgedBrieIncreasesInQualityByTwoAfterItsExpirationDay() {
       Item[] items = new Item[] { new Item(AGED_BRIE, -1, 40)};
-      GildedRose app = new GildedRose(items);
+      app = new GildedRose(items);
       app.updateQuality();
       assertThat(app.items[0].quality, is(42));
     }
@@ -59,7 +60,7 @@ public class GildedRoseTest {
     @Test
     public void ctAgedBrieIncreasesInQualityByOneBeforeItsExpirationDay() {
       Item[] items = new Item[] { new Item(AGED_BRIE, 1, 40)};
-      GildedRose app = new GildedRose(items);
+      app = new GildedRose(items);
       app.updateQuality();
       assertThat(app.items[0].quality, is(41));
     }

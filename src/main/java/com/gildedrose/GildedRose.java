cec2e1718a8ff29e4bc413503686214b9c2f7bfd
Rich Dammkoehler <rpd@noradltd.com>
2015-03-02 10:23:55 -0600

    extracted all constant variations of item modifier

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,6 +9,26 @@ class GildedRose {
   private static final int QUALITY_FLOOR = 0;
   private static final int QUALITY_CEILING = 50;
 
+  private static final ItemModifier AGELESS_ITEM_MODIFIER = new ItemModifier(0, 0);
+  private static final ItemModifier BACKSTAGE_PASS_ITEM_MODIFIER = new ItemModifier(DEFAULT_QUALITY_INCREASE_AMOUNT, -1) {
+    public void step(Item item) {
+      adjustQuality(item);
+      if (item.sellIn < 11) {
+        adjustQuality(item);
+      }
+      if (item.sellIn < 6) {
+        adjustQuality(item);
+      }
+      adjustSellIn(item);
+      if (pastSellBy(item)) {
+        item.quality = 0;
+      }
+    }
+  };
+  private static final ItemModifier BRIE_ITEM_MODIFIER = new ItemModifier(DEFAULT_QUALITY_INCREASE_AMOUNT, -1);
+  private static final ItemModifier DECAYING_ITEM_MODIFIER = new ItemModifier(DEFAULT_QUALITY_DECAY_AMOUNT, -1);
+  private static final ItemModifier DOUBLE_DECAYING_ITEM_MODIFIER = new ItemModifier(2 * DEFAULT_QUALITY_DECAY_AMOUNT, -1);
+
   private static final String AGED_BRIE = "Aged Brie";
   private static final String BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
   private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
@@ -31,33 +51,19 @@ class GildedRose {
 
   private ItemModifier selectModifierFor(Item item) {
     if (AGED_BRIE.equals(item.name)) {
-      return new ItemModifier(DEFAULT_QUALITY_INCREASE_AMOUNT, -1);
+      return BRIE_ITEM_MODIFIER;
     } else if (BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT.equals(item.name)) {
-      return new ItemModifier(DEFAULT_QUALITY_INCREASE_AMOUNT, -1) {
-        public void step(Item item) {
-          adjustQuality(item);
-          if (item.sellIn < 11) {
-            adjustQuality(item);
-          }
-          if (item.sellIn < 6) {
-            adjustQuality(item);
-          }
-          adjustSellIn(item);
-          if (pastSellBy(item)) {
-            item.quality = 0;
-          }
-        }
-      };
+      return BACKSTAGE_PASS_ITEM_MODIFIER;
     } else if (SULFURAS_HAND_OF_RAGNAROS.equals(item.name)) {
-      return new ItemModifier(0, 0);
+      return AGELESS_ITEM_MODIFIER;
     } else if (CONJURED_MANA_CAKE.equals(item.name)) {
-      return new ItemModifier(2 * DEFAULT_QUALITY_DECAY_AMOUNT, -1);
+      return DOUBLE_DECAYING_ITEM_MODIFIER;
     } else {
-      return new ItemModifier(DEFAULT_QUALITY_DECAY_AMOUNT, -1);
+      return DECAYING_ITEM_MODIFIER;
     }
   }
 
-  class ItemModifier {
+  static class ItemModifier {
     private Integer qualityAdjustment = DEFAULT_QUALITY_DECAY_AMOUNT;
     private Integer sellInAdjustment = -1;
 
